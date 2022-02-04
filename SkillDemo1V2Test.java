c1fd4aeb8ee87dc86029c3b1b0184d726196a424
sm52 <sakina.muckadam@gmail.com>
2022-02-03 17:05:51 -0800

    fixed the wrong test

--- SkillDemo1V2Test.java
+++ SkillDemo1V2Test.java
@@ -4,6 +4,6 @@ import org.junit.*;
 public class SkillDemo1V2Test {
     @Test
     public void test1(){
-        assertEquals(5, SkillDemo1V2.multiply(2));
+        assertEquals(4, SkillDemo1V2.multiply(2));
     }
 }
