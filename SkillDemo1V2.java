0043b597c84401faa90708e6bcfaca37f22e4feb
sm52 <sakina.muckadam@gmail.com>
2022-02-03 17:03:10 -0800

    tester file and wrong test

--- SkillDemo1V2.java
+++ SkillDemo1V2.java
@@ -1,1 +1,5 @@
-
+public class SkillDemo1V2 {
+    public static int multiply(int n){
+        return n * 2;
+    }
+}
---
+++ SkillDemo1V2Test.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemo1V2Test {
+    @Test
+    public void test1(){
+        assertEquals(5, SkillDemo1V2.multiply(2));
+    }
+}
